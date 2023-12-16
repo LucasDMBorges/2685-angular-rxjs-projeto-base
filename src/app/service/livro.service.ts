432ed8691a6dd7d8394da56a6f4b569501aaeab9
LucasDMBorges <dev.lucasdean@gmail.com>
2023-12-16 16:39:08 -0300

    feat: :sparkles: RxJs para tratamento de resposta API

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,18 +1,25 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
+import { Item, ResultBooks } from '../models/interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LivroService {
-  private readonly API =
-    'https://www.googleapis.com/books/v1/volumes?q=search+terms';
+  private readonly API = 'https://www.googleapis.com/books/v1/volumes';
+
+  query = 'Angular';
 
   constructor(private http: HttpClient) {}
 
-  find(query: string): Observable<any> {
-    const params = new HttpParams().append('q', query);
-    return this.http.get(this.API, { params });
+  find(query?: string): Observable<Item[]> {
+    if (query) this.query = query;
+    const params = new HttpParams().append('q', this.query);
+
+    return this.http.get<ResultBooks>(this.API, { params }).pipe(
+      tap((retornoApi) => console.log(retornoApi)),
+      map((resultado) => resultado.items)
+    );
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { ResultBooks, VolumeInfo, ImageLinks } from './../../models/interface';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Book } from 'src/app/models/interface';
+
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -6,14 +10,43 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent {
-  listaLivros: [];
+export class ListaLivrosComponent implements OnDestroy {
+  listBoook: Book[];
 
   query: string = '';
 
+  subscription: Subscription;
+
+  book: Book;
+
   constructor(private service: LivroService) {}
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
   findBooks() {
-    this.service.find(this.query).subscribe((res) => console.log(res));
+    this.subscription = this.service.find(this.query).subscribe({
+      next: (res) => (this.listBoook = this.formatResultBooks(res)),
+      error: (err) => console.log(err),
+    });
+  }
+
+  formatResultBooks(resultBooks): Book[] {
+    const listBooks: Book[] = [];
+    resultBooks.forEach((book) => {
+      listBooks.push(
+        (this.book = {
+          title: book.volumeInfo?.title,
+          authors: book.volumeInfo?.authors,
+          publisher: book.volumeInfo?.publisher,
+          publishedDate: book.volumeInfo?.publishedDate,
+          description: book.volumeInfo?.description,
+          previewLink: book.volumeInfo?.previewLink,
+          thumbnail: book.volumeInfo?.ImageLinks?.thumbnail,
+        })
+      );
+    });
+    return listBooks;
   }
 }
